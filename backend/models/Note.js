a0cb52186ea1a887c1e12aa37313b247d5527fe9
pramodpharande07 <pramodpharande2113@gmail.com>
2023-10-29 10:38:44 +0530

    React setup completed

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,32 +1,3 @@
-// const mongoose = require('mongoose')
-// const { Schema } = mongoose;
-
-// const NotesSchema = new Schema({
-//     user:{
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref:'user'
-//     },
-//     // above code is for - user should not able to read each other notes
-//     title: {
-//       type: String,
-//       required: true
-//     },
-//     description: {
-//         type: String,
-//         required: true,
-//       },
-//       tag: {
-//         type: String,
-//         default: "general"
-//       },
-//       date: {
-//         type: Date,
-//         default: Date.now
-//       }
-
-// })
-// module.exports = mongoose.model('notes', NotesSchema);
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
